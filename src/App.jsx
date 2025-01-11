4c2af3f6fc3dd159f7f2ee84f5c8d4ff1411c58f
Royaltechsis <osenibunmi2023@gmail.com>
2025-01-11 19:31:49 +0100

    router issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import Home from "./pages/home"; // Your existing Home component
 import Resume from "./pages/resume"; // Import the Resume component
 import './index.css'; // Ensure this includes Tailwind CSS imports
